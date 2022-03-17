831d6c0e0fb87a2086d176b1c529d38c345963df
Damian Kozyra <d.kozyra@hotmail.co.uk>
2022-03-17 17:09:15 +0000

    configure sequelize

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const indexRouter = require("./routes");
+const db = require("./models");
 
 const app = express();
 
+app.use(express.json());
 app.use("/", indexRouter);
 
-app.listen(5000, () => {
-  console.log("listening on port 5000");
+db.Sequelize.sync().then(() => {
+  app.listen(5000, () => {
+    console.log("listening on port 5000");
+  });
 });
 
 module.exports = app;
---
+++ server/models/Posts.js
@@ -0,0 +1,18 @@
+module.exports = (Sequelize, DataTypes) => {
+  const Posts = Sequelize.def("Posts", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    postText: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  return Posts;
+};
