40df0f4e3c2a45dd39e4b0d37bbf29da416dc938
Damian Kozyra <d.kozyra@hotmail.co.uk>
2022-03-18 16:12:55 +0000

    complete get all posts and display them in the browser

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [postsList, setPostsList] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/posts").then((response) => {
+      setPostsList(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {postsList.map((value, key) => {
+        return (
+          <div className="post" key={value.id}>
+            <div className="title">{value.title}</div>
+            <div className="body">{value.postText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
